b4f2c50397bc61cea8cf003d8f124c5a24a36b9e
zakkesu <83237975+zakjeg@users.noreply.github.com>
2025-04-27 20:17:29 +0200

    povezava z mysql, dodal .gitignore za povezavo v .txt

--- Views/UserListView.xaml.cs
+++ Views/UserListView.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +15,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Diagnostics;
 
 namespace marjetaUredi.Views
 {
@@ -23,6 +28,35 @@ namespace marjetaUredi.Views
         public UserListView()
         {
             InitializeComponent();
+
+            string connectionCredentials = (System.IO.Path.Combine(Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName, "Assets") + System.IO.Path.DirectorySeparatorChar) + "databaseConnection.txt";
+            if (File.Exists(connectionCredentials))
+            {
+                Debug.WriteLine($"File found - {connectionCredentials}");
+            }
+            else
+            {
+                Debug.WriteLine($"File not found - {connectionCredentials}");
+            }
+
+
+
+
+
+            string connectionString = File.ReadAllText(connectionCredentials);
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+
+            MySqlCommand command = new MySqlCommand("SELECT * FROM users", connection);
+
+            connection.Open();
+
+            DataTable dataTable = new DataTable();
+            dataTable.Load(command.ExecuteReader());
+
+            connection.Close();
+
+            dtGrid.DataContext = dataTable; 
         }
     }
 }
